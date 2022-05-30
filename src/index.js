35faaebce85a03cf5588678b54ac7dc593b6c839
nybaustin <austin.ngyubin@gmail.com>
2022-05-30 20:31:46 +0800

    Read Leagues from json

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import styles from './style.module.css';
 import { render } from "react-dom";
 import reportWebVitals from './reportWebVitals';
-import Dropdown from './List.js';
+import Dropdown from './Dropdown';
 import Dropdown2 from './List2.js';
 import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -16,10 +17,32 @@ class App extends Component {
         name: "React",
         showLeague: true,
         showSeason: true,
+        leagueData: []
       };
       this.hideComponent = this.hideComponent.bind(this);
     }
   
+    componentWillMount(){
+      this.getLeagueData()
+    }
+    getLeagueData(){
+      fetch('league.json'
+      ,{
+        headers : { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+         }
+      }
+      )
+        .then(response => {
+          console.log(response)
+          return response.json();
+        })
+        .then(myJson => {
+          this.setState({leagueData: myJson.response})
+          console.log(myJson);
+        });
+    }
     hideComponent(name) {
       console.log(name);
       switch (name) {
@@ -38,19 +61,68 @@ class App extends Component {
     render() {
       const { showLeague, showSeason} = this.state;
       return (
-        <div style={{display: "inline"}}>
-          {showLeague && <Dropdown onClick={() => this.hideComponent("showHideDemo2")}/>}
+        <div className={styles.header} >
+        <div >
+          {/* {showLeague && <Dropdown style={{color:"blue"}} onClick={() => this.hideComponent("showHideDemo2")}/>} */}
+          {/* <Dropdown label={"league"} value={""} options={this.state.leagueData.map(leagueObj => {return { value: leagueObj.name, label:leagueObj.name }} )} onChange={() => this.hideComponent("showHideDemo2")} /> */}
+          {/* {this.state.leagueData.map((leagueObj,key) => {
+          return <div>{leagueObj.league.name}-{leagueObj.country.name}-{leagueObj.seasons.map(seasonObj=> seasonObj.year+",")}</div>
+        })} */}
+        
+          <select className='leagueDrop'>
+            <option>Select League</option>
+            {this.state.leagueData.map((leagueObj,key) => {
+            return <option>{leagueObj.league.name}</option>
+            
+        })}
+        </select>
+        <select className='seasonDrop'>
+            <option>Select Year</option>
+            {this.state.leagueData.map((leagueObj,key) => {
+            return <option>{leagueObj.seasons.year}</option>
+            
+        })}
+        </select>
           
-          
-          
-          <div style={{display: "inline"}}>
+          {/* <div style={{display: "inline"}}>
           {showSeason && <Dropdown2 />}  
                       
-          </div>
-          <div style={{display: "inline",alignItems:"right"}}>  
+          </div> */}
+          <div className={styles.buttonDiv } >  
             <Button onClick={() => this.hideComponent("showHideDemo2")} variant="primary">Button #1</Button>
           </div>
         </div>
+        {this.state.leagueData.map((leagueObj,key) => {
+          return <div>{leagueObj.league.name}-{leagueObj.country.name}-{leagueObj.seasons.map(seasonObj=> seasonObj.year+",")}</div>
+        })}
+        <div style={{display:"inline"}}>
+          <div className={styles.listview} style={{height: "100%",width:"50%"}}>
+            <div >
+              <h2>Column 1</h2>
+              
+              <p>Some text..</p>
+            </div>
+            <br style={{clear:"both"}}/>
+            <div >
+              <h2>Column 2</h2>
+              <p>Some text..</p>
+            </div>
+
+            <div >
+              <h2>Column 3</h2>
+              <p>Some text..</p>
+            </div>
+            <div>
+              <h2>Column 4</h2>
+              <p>Some text..</p>
+            </div>
+          </div>
+            <div style={{height: "100%",width:"50%"}}>
+              Content
+            </div>
+          </div>
+        </div>
+        
       );
     }
   }
